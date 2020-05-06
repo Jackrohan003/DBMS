9e252d3e29817b2cde335dbde7f9b2a018e16fd0
Rohan Prajapati <58733914+Jackrohan003@users.noreply.github.com>
2020-05-06 11:58:23 +0530

    Create Revise.sql

---
+++ Revise.sql
@@ -0,0 +1,8 @@
+#List the city, region and amount target for each office.
+select city,regition, (Sales-Target) from Office.
+
+select  ID,Name ,(Quantity * Price) from products
+
+
+Select city, 'has sales of ', sales from Office
+
