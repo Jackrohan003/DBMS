40b80e548aec17b836299f7d9419a2696f780743
Rohan Prajapati <58733914+Jackrohan003@users.noreply.github.com>
2021-02-05 11:25:56 +0530

    Update GeneralQuery.sql

--- GeneralQuery.sql
+++ GeneralQuery.sql
@@ -2,3 +2,7 @@
 update tbl_email_template set notification_parameters = CONCAT(SUBSTRING(notification_parameters, 1, LEN(notification_parameters)-1),',"inspectorCellPhoneNumber"]'),
 updated_at= GETUTCDATE() where notification_event_type_id = 22 and notification_type_id=3;
 
+/*In clause in SQL */
+select * from tbl_email_template where notification_event_type_id IN (45,46,47,62) and notification_type_id=3
+
+
